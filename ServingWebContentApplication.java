b2b8733b202242f3142344a8ed336b9fa9336ffd
blissfish <mark.evertz@gmx.de>
2020-01-04 13:01:04 +0100

    Add files via upload

---
+++ ServingWebContentApplication.java
@@ -0,0 +1,18 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
